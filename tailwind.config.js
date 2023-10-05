b0f4f0326ed5658c20a75ec75b76277e8caa6eb9
Imran Hossain <imran.cit.bd@gmail.com>
2023-10-06 05:01:34 +0600

    nav banner about compossion part dont

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          'btn-color': '#FFD2A4',
+          'font-color': '#000',
+          'box-color': '#FFF5EB',
+          'after-color': '#FFD0A0',
+        },
+        fontSize: {
+        'bg-font': '64px',
+        'heading-font': '48px',
+        'subheading-font': '32px',
+        'pera-font': '16px',
+        },
+        fontFamily: {
+            'robota': "'Roboto Condensed', sans-serif",
+        },
+        maxWidth: {
+            'container': '1280px',
+        },
+        backgroundImage: {
+            'banner': "url('/images/banner.jpg')",
+        },
+        spacing: {
+            'btn-padding': '20px 48px',
+            'btn-2': '20px 64px',
+        }
+      }
+    }
+  }
