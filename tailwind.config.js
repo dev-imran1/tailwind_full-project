c003954786f193fb5bc067af3903914e51b4ae3a
Imran Hossain <imran.cit.bd@gmail.com>
2023-10-11 01:39:36 +0600

    full desing correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,12 +20,17 @@ tailwind.config = {
             'container': '1280px',
         },
         backgroundImage: {
-            'banner': "url('./images/banner.jpg')",
-            'banner3': "url('./images/banner3.jpg')",
+            'banner': "linear-gradient(184deg, #060606 16.68%, rgba(0, 0, 0, 0.5) 90.44%),url('./images/banner3.jpg')",
+            'benifit1': "url('./images/benifit1.jpg')",
+            'benifit2': "url('./images/benifit2.jpg')",
+            'benifit3': "url('./images/benift3.jpg')",
+            'benifit4': "url('./images/benifit4.jpg')",
+            'benifit-overlay': "linear-gradient(184deg, #060606 16.68%, rgba(0, 0, 0, 0.001) 90.44%)",
             'arround': "url('./images/next.jpg')",
             'overlay': "linear-gradient(184deg, #060606 16.68%, rgba(0, 0, 0, 0.07) 50.44%)",
             'banner-overlay': "linear-gradient(184deg, #060606 16.68%, rgba(0, 0, 0, 0.001) 90.44%)",
             'font-linear': "linear-gradient(110deg, #A54E2B -53.83%, #DC9853 62.61%)",
+
         },
         spacing: {
             'btn-padding': '20px 48px',
